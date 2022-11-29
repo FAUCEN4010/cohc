b345185851adb0c66a6ad19fa25d6c273f01473f
z3301 <98485231+z3301@users.noreply.github.com>
2022-11-29 10:18:43 -0500

    paid render

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -58,7 +58,7 @@ async function login(req, res) {
         httpOnly: true,
         sameSite: false,
         secure: true,
-        domain: "cohc-server.onrender.com",
+        domain: "cohc.onrender.com",
         //secure: process.env.NODE_ENV === "production",  
     });
     console.log("token in usersController: ", token);
