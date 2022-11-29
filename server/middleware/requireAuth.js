c6ecd1ce25f77440a07c07af696ae8838432c96f
z3301 <98485231+z3301@users.noreply.github.com>
2022-11-28 20:49:42 -0500

    deployment commit

--- server/middleware/requireAuth.js
+++ server/middleware/requireAuth.js
@@ -5,12 +5,15 @@ async function requireAuth(req, res, next) {
     try{
     // read token off cookieParser
     const token = req.cookies.Authorization;
+    console.log(token);
 
     // decode the token 
     const decoded = jwt.verify(token, process.env.SECRET);
+    console.log(decoded);
 
     // check expiration
     if (decoded.exp < Date.now()) return res.status(401).send("Token expired");
+    console.log("Token is valid"); 
 
 
     // find user decoded sub
